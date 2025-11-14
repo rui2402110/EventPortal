1883e21ec6d17431a016b3f99ff35cd866bee165
rui2402110 <tky2402110@stu.o-hara.ac.jp>
2025-11-14 09:38:55 +0900

    参加可能イベント取得　新橋

--- src/dao/EventDao.java
+++ src/dao/EventDao.java
@@ -151,4 +151,74 @@ public class EventDao extends Dao {
 
 	}
 
+	// ユーザーIDを使用して、ユーザーIDが合致するイベントを全て取得するメソッド
+	public List<Event> joinedFilter() throws Exception {
+		// Eventクラスを再定義
+		Event event = new Event();
+		// コネクションを確立
+		Connection connection = getConnection();
+		// プリペアードステートメント
+		PreparedStatement statement = null;
+
+		// リストを定義
+		List<Event> list = new ArrayList<>();
+
+		try {
+			// プリペアードステートメントにSQL文をセット
+			statement = connection.prepareStatement("SELECT * FROM EVENTS WHERE EVENT_HOLD_STATE =1");
+			// SQL文の実行
+			ResultSet resultSet = statement.executeQuery();
+			if (resultSet.next()) {
+				// SELECTしたデータをインスタンスにセット
+			    event.setEventId(resultSet.getString("event_id"));
+			    event.setEventName(resultSet.getString("event_name"));
+			    event.setEventOverview(resultSet.getString("event_overview"));
+			    event.setHoldingDate(resultSet.getDate("holding_date") != null ? resultSet.getDate("holding_date").toLocalDate() : null);
+			    event.setHoldingTime(resultSet.getTime("holding_time") != null ? resultSet.getTime("holding_time").toLocalTime() : null);
+			    event.setAddress(resultSet.getString("address"));
+			    event.setMapOutOfHall(resultSet.getString("map_out_of_hall"));
+			    event.setMapInHall(resultSet.getString("map_in_hall"));
+			    event.setMaxCount(resultSet.getInt("max_count"));
+			    event.setCategoryId(resultSet.getString("category_id"));
+			    event.setPhoneNumber(resultSet.getString("phone_number"));
+			    event.setLink(resultSet.getString("link"));
+			    event.setCredit(resultSet.getString("credit"));
+			    event.setUserId(resultSet.getString("user_id"));
+			    event.setTicketInfo(resultSet.getString("ticket_info"));
+			    event.setEventHoldState(resultSet.getString("event_hold_state"));
+			    event.setEventAddDate(resultSet.getDate("event_add_date") != null ? resultSet.getDate("event_add_date").toLocalDate() : null);
+			    event.setTotalPayment(resultSet.getInt("TOTAL_PAYMENT"));
+
+			    // 作成したインスタンスをリストに格納
+			    list.add(event);
+			} else {
+				// リザルトセットが存在しない場合
+				// インスタンスにnullをセット
+				event = null;
+			}
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			// プリペアードステートメントを閉じる
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			// コネクションを閉じる
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+		return list;
+
+	}
+
 }
